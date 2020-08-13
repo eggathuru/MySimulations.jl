b237fe551ab2f0fb2a058de27acc8b497c2a238e
Edward Gathuru <eggathuru@pm.me>
2020-08-12 21:56:45 -0500

    Examples to generate plots.

--- examples/ensemble_tau_bd.jl
+++ examples/ensemble_tau_bd.jl
@@ -2,35 +2,38 @@ using MySimulations, Plots, Statistics
 
 # Simulation parameters
 n_days = 1000
-n_ensemble = 30
+n_ensemble = 100
 μ = 0.02 # birth/death
 
 # Independent variables
-N = 10
+N₀ = 10
 τ = 0.05
 
 # Run simulations
-prb = MyBirthDeath(N, μ)
+prb = MyBirthDeath(N₀, μ)
 e = ensemble_tau!(prb, n_days, τ, n_ensemble)
 
 # Plot ensemble
-p = plot(xlabel = "Time",
+p = plot(xlabel = "Days",
          ylabel = "People",
          xlims = (0, n_days),
          # ylims = (0, 2*N),
          legend=false,
-         title="Birth Death Ensemble")
+         title="Birth Death ($n_ensemble runs, N₀=$N₀, τ=$τ)")
 
-A = zeros(Float64, n_ensemble, n_days)
+A = zeros(Int, n_ensemble, n_days)
 T = e[1].T
 
 for i in 1:n_ensemble
-    A[i, :] .= e[i].S
+    global A[i, :] .= e[i].S
     plot!(p, T, A[i, :])
 end
+
+ylims!(p, 0, maximum(A))
 
 # Plot mean and error
 μs = mean(A, dims=1)[1, :]
 σs =  std(A, dims=1)[1, :]
 
 plot!(p, T, μs, yerr=σs, alpha=0.1)
+savefig(p, "./images/bd_N$(N₀)_t$(τ).png")
--- examples/ensemble_tau_seir.jl
+++ examples/ensemble_tau_seir.jl
@@ -2,7 +2,7 @@ using MySimulations, Plots, Statistics
 
 # Simulation parameters
 n_days = 2000
-n_ensemble = 30
+n_ensemble = 20
 μ = 0.02 # birth/death
 β = 0.055 # exposure
 a = 0.07 # onset
@@ -11,7 +11,7 @@ a = 0.07 # onset
 
 # Independent variables
 N = 100 # S₀ E₀ I₀ R₀
-τ = 0.05
+τ = 0.99
 
 # Run simulations
 prb = MySEIR(N*[0.9, 0, 0.1, 0],
@@ -19,36 +19,47 @@ prb = MySEIR(N*[0.9, 0, 0.1, 0],
 e = ensemble_tau!(prb, n_days, τ, n_ensemble)
 
 # Plot ensemble
-p1 = plot(xlabel = "Time",
+p1 = plot(xlabel = "Days",
           ylabel = "Susceptables",
           xlims = (0, n_days),
           legend=false,
           title="Susceptable population")
-p2 = plot(xlabel = "Time",
+p2 = plot(xlabel = "Days",
           ylabel = "Infecteds",
           xlims = (0, n_days),
           legend=false,
           title="Infected population")
 
-M_S = zeros(Float64, n_ensemble, n_days)
-M_I = zeros(Float64, n_ensemble, n_days)
-M_R = zeros(Float64, n_ensemble, n_days)
+M_S = zeros(Int, n_ensemble, n_days)
+M_E = zeros(Int, n_ensemble, n_days)
+M_I = zeros(Int, n_ensemble, n_days)
+M_R = zeros(Int, n_ensemble, n_days)
 T = e[1].T
 
 for i in 1:n_ensemble
     M_S[i, :] .= e[i].S
+    M_E[i, :] .= e[i].E
     M_I[i, :] .= e[i].I
     M_R[i, :] .= e[i].R
     plot!(p1, T, M_S[i, :])
     plot!(p2, T, M_I[i, :])
 end
 
-plot(p1, p2)
+y_max = max(maximum(M_S),
+            maximum(M_I))
+ylims!(p1, 0, y_max)
+ylims!(p2, 0, y_max)
 
+p_ens = plot(p1, p2)
+# Save plot
+# savefig(p_ens, "./images/seir_N$(N)_t$(τ)_ens.png")
 
 # Plot mean and error
 μSs = mean(M_S, dims=1)[1, :]
 σSs =  std(M_S, dims=1)[1, :]
+
+μEs = mean(M_E, dims=1)[1, :]
+σEs =  std(M_E, dims=1)[1, :]
 
 μIs = mean(M_I, dims=1)[1, :]
 σIs =  std(M_I, dims=1)[1, :]
@@ -56,24 +67,38 @@ plot(p1, p2)
 μRs = mean(M_R, dims=1)[1, :]
 σRs =  std(M_R, dims=1)[1, :]
 
+y_max = max(maximum(μSs+σSs),
+            maximum(μEs+σEs),
+            maximum(μIs+σIs),
+            maximum(μRs+σRs))
 pS = plot(T, μSs, yerr=σSs,
                   alpha=0.1,
-                  label='S')
+                  label='S',
+                  ylims=(0, y_max))
+pE = plot(T, μEs, yerr=σEs,
+                  alpha=0.1,
+                  label='E',
+                  ylims=(0, y_max))
 pI = plot(T, μIs, yerr=σIs,
                   alpha=0.1,
-                  label='I')
+                  label='I',
+                  ylims=(0, y_max))
 pR = plot(T, μRs, yerr=σRs,
                   alpha=0.1,
-                  label='R')
-plot(pS, pI, pR)
+                  label='R',
+                  ylims=(0, y_max))
+p_var = plot(pS, pE, pI, pR)
 
+# Save plot
+# savefig(p_var, "./images/seir_N$(N)_t$(τ)_var.png")
 
 # Plot means on one plot
-plot(T, [μSs μIs μRs],
-     label=['S' 'I' 'R'],
-     xlabel = "Time",
-     ylabel = "People",
-     title="SEIR Stochastic Simulation",
-     linetype=:steppost)
+p_mean = plot(T, [μSs μEs μIs μRs],
+              label=['S' 'E' 'I' 'R'],
+              xlabel = "Time",
+              ylabel = "People",
+              title="SEIR Stochastic Simulation",
+              linetype=:steppost)
 
-# savefig("./images/plot.png")
+# Save plot
+# savefig(p_mean, "./images/seir_N$(N)_t$(τ)_mean.png")
