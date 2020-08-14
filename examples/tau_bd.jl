b6543febee01d3fb5bf70475cdcfce12a82d70a1
Edward Gathuru <eggathuru@pm.me>
2020-08-13 19:23:35 -0500

    New tau leaping examples.

--- examples/tau_bd.jl
+++ examples/tau_bd.jl
@@ -2,18 +2,13 @@ using MySimulations
 
 # Simulation parameters
 n_days = 1000
-μ = 0.02 # birth/death
-β = 0.055 # exposure
-a = 0.07 # onset
-γ = 0.006 # recovery
-α = 0.001 # infection death
+μ = 0.01 # birth/death
 
 # Independent variables
 N = 100 # S₀ E₀ I₀ R₀
 τ = 0.5
 
-prb = MySEIR(N*[0.9, 0, 0.1, 0],
-                (μ, β, a, γ, α))
+prb = MyBirthDeath(N, μ)
 tau_leap!(prb, n_days, τ)
 direct_ssa!(prb, Int(2e3))
 
