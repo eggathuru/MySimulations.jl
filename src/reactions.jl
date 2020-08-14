eaf75dbdce1e70bec7c68e8a2986ae8cfd52ef03
Edward Gathuru <eggathuru@pm.me>
2020-08-14 18:35:57 -0500

    Fixed total reaction counter.

--- src/reactions.jl
+++ src/reactions.jl
@@ -1,43 +1,43 @@
 function birth!(data, i, v = 1)
     data.S[i] += v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
 
 function susceptible_to_exposed!(data, i, v = 1)
     data.S[i] -= v
     data.E[i] += v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
 
 function exposed_to_infectious!(data, i, v = 1)
     data.E[i] -= v
     data.I[i] += v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
 
 function infectious_to_resistant!(data, i, v = 1)
     data.I[i] -= v
     data.R[i] += v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
 
 # Death events
 function susceptible_death!(data, i, v = 1)
     data.S[i] -= v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
 
 function exposed_death!(data, i, v = 1)
     data.E[i] -= v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
 
 function infected_death!(data, i, v = 1)
     data.I[i] -= v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
 
 function recovered_death!(data, i, v = 1)
     data.R[i] -= v
-    if v > 0 data.total_reactions += 1 end
+    data.total_reactions += v
 end
